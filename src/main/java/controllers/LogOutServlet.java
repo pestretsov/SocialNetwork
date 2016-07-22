74b55fcd72f21c01fb77c9faa86fa49b8196b98a
Artemy Pestretsov <pestretsov@me.com>
2016-07-22 03:50:57 +0300

    add LogOutServlet

--- src/main/java/controllers/LogOutServlet.java
+++ src/main/java/controllers/LogOutServlet.java
@@ -1,7 +1,34 @@
 package controllers;
 
+import dao.interfaces.UserDAO;
+import utils.SecurityUtils;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
 /**
  * Created by artemypestretsov on 7/22/16.
  */
-public class LogOutServlet {
+
+@WebServlet(urlPatterns = "/logout")
+public class LogOutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.removeAttribute("user");
+
+        resp.sendRedirect("/");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,11 +6,13 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
   <head>
     <title></title>
   </head>
   <body>
     <h1>Welcome!!!</h1>
+    <a href="<c:url value="/logout"/>">Logout</a>
   </body>
 </html>
