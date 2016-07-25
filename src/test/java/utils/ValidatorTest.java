511102101b6a336aaaf1092b271329e0ab7a035e
Artemy Pestretsov <pestretsov@me.com>
2016-07-25 14:44:11 +0300

    test Validator class

--- src/test/java/utils/ValidatorTest.java
+++ src/test/java/utils/ValidatorTest.java
@@ -1,7 +1,39 @@
 package utils;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 /**
  * Created by artemypestretsov on 7/25/16.
  */
 public class ValidatorTest {
+    @Test
+    public void testUsernameOnlyWithLetters() throws Exception {
+        String username = "ambush";
+        assertTrue(Validator.validateUsername(username));
+    }
+
+    @Test
+    public void testUsernameOnlyWithNumbers() throws Exception {
+        String username = "1232123";
+        assertTrue(Validator.validateUsername(username));
+    }
+
+    @Test
+    public void testUsernameOf2Symbols() throws Exception {
+        String username = "ss";
+        assertFalse(Validator.validateUsername(username));
+    }
+
+    @Test
+    public void testUsernameOf16Symbols() throws Exception {
+        String username = "1234567890123456";
+        assertFalse(Validator.validateUsername(username));
+    }
+
+    @Test
+    public void testMixedUsernameOf15Symbols() throws Exception {
+        String username = "12_12_SSSsZ0Amm";
+        assertTrue(Validator.validateUsername(username));
+    }
 }
