3104880f64a42a4261e19aec91f5077459d49c9d
Artemy Pestretsov <pestretsov@me.com>
2016-07-25 14:38:24 +0300

    add username validation

---
+++ src/main/java/utils/Validator.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by artemypestretsov on 7/25/16.
+ */
+public class Validator {
+
+    // digits, underscore, letters (upper- and lowercase)
+    private static final String usernamePatternString = "^\\w{3,15}$";
+    private static final Pattern usernamePattern = Pattern.compile(usernamePatternString);
+
+    public static boolean validateUsername(String username) {
+        return usernamePattern.matcher(username).matches();
+    }
+}
