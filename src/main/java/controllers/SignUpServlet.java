731a12cdfdf82f63de2c1e2b68cd8b9f2142119a
Artemy Pestretsov <pestretsov@me.com>
2016-07-28 18:30:00 +0300

    fix paths in getRequestDispatcher() calls

--- src/main/java/controllers/SignUpServlet.java
+++ src/main/java/controllers/SignUpServlet.java
@@ -33,7 +33,7 @@ public class SignUpServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("signup.jsp").forward(req, resp);
+        req.getRequestDispatcher("/signup.jsp").forward(req, resp);
     }
 
     @Override
