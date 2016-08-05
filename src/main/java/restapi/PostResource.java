2b80d22be4ead3ae181858befa4d941957b64468
Artemy Pestretsov <pestretsov@me.com>
2016-08-05 15:12:46 +0300

    add logging to PostResource

--- src/main/java/restapi/PostResource.java
+++ src/main/java/restapi/PostResource.java
@@ -3,6 +3,7 @@ package restapi;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.interfaces.PostDAO;
+import lombok.extern.slf4j.Slf4j;
 import model.Post;
 
 import javax.servlet.ServletContext;
@@ -20,6 +21,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
  * Created by artemypestretsov on 7/29/16.
  */
 
+@Slf4j
 @Path("/posts")
 public class PostResource {
     private static PostDAO postDAO;
@@ -40,16 +42,21 @@ public class PostResource {
     @Produces(APPLICATION_JSON)
     public Response getPostById(@PathParam("id") int id) {
 
+        log.info("trying to GET post with postId={}", id);
+
         Optional<Post> postOpt = postDAO.getById(id);
 
         try {
             if (postOpt.isPresent()) {
+                log.info("got post with postId={}", id);
                 String json = toJson(postOpt.get());
                 return Response.ok(json).build();
             } else {
+                log.warn("no such post with postId={}", id);
                 return Response.noContent().build();
             }
         } catch (JsonProcessingException e) {
+            log.warn("JsonProcessingException thrown trying to get post with postId={}", id);
             throw new RuntimeException(e);
         }
     }
@@ -74,9 +81,12 @@ public class PostResource {
     @DELETE
     @Path("{id}")
     public void deletePostById(@PathParam("id") int id) {
+        log.info("trying to DELETE post with postId={}", id);
         if (postDAO.getById(id).isPresent()) {
+            log.info("post with postId={} was deleted", id);
             postDAO.deleteById(id);
         } else {
+            log.info("no such post with postId={}", id);
             throw new RuntimeException("Resource with such path is unavailable");
         }
     }
@@ -84,6 +94,7 @@ public class PostResource {
     @PUT
     @Consumes(APPLICATION_JSON)
     public void updatePostById(Post post) {
+        log.info("trying to UPDATE post with postId={}", post.getId());
         // TODO: needs fix -- probably new parser
         post.setPublishTime(Instant.now());
         postDAO.update(post);
