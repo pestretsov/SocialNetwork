147c758d0e1c74895563b158c03d247ffc3698f3
Artemy Pestretsov <pestretsov@me.com>
2016-08-10 02:36:40 +0300

    add CommentResource

--- src/main/java/restapi/CommentResource.java
+++ src/main/java/restapi/CommentResource.java
@@ -1,15 +1,22 @@
 package restapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import dao.interfaces.CommentDAO;
+import dao.interfaces.CommentViewDAO;
 import lombok.extern.slf4j.Slf4j;
+import model.*;
 
 import javax.servlet.ServletContext;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static utils.RestUtils.toJson;
@@ -21,13 +28,21 @@ import static utils.RestUtils.toJson;
 @Path("/comments")
 public class CommentResource {
     private static CommentDAO commentDAO;
+    private static CommentViewDAO commentViewDAO;
 
     @Context
     public void init(ServletContext servletContext) {
         if (commentDAO == null) {
             commentDAO = (CommentDAO) servletContext.getAttribute("commentDAO");
         }
+
+        if (commentViewDAO == null) {
+            commentDAO = (CommentDAO) servletContext.getAttribute("commentViewDAO");
+        }
     }
+
+    @Context
+    HttpServletRequest request;
 
     @GET
     @Path("/getcomments")
@@ -35,7 +50,47 @@ public class CommentResource {
     public Response getPostComments(@QueryParam("postId") int postId,
                                     @QueryParam("offsetId") int offsetId,
                                     @QueryParam("limit") int limit) {
-        return Response.ok().build();
+
+        // TODO: post-privacy & follow
+        List<CommentView> comments = commentViewDAO.getPostCommentsSublist(postId, offsetId, limit);
+
+        try {
+            String json = toJson(comments);
+            log.debug("someone requested comments to postId={}", postId);
+            return Response.ok(json).build();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @POST
+    @Path("/addcomment")
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    public Response addComment(Comment comment) {
+
+        HttpSession session = request.getSession(false);
+
+        Optional<User> sessionUserOpt = Optional.ofNullable(session)
+                .map(s -> (User)s.getAttribute("sessionUser"));
+
+        if (!sessionUserOpt.isPresent()) {
+            log.warn("No user session found. Cannot add comment");
+            return Response.status(Response.Status.FORBIDDEN)
+                    .entity("User is not signedup").build();
+        }
+
+        try {
+            log.info("userId={} is trying to add comment to postId={}", comment.getFromId(), comment.getPostId());
+            if (commentDAO.addPostComment(comment) >= 0) {
+                log.info("userId={} has added comment to postId={}", comment.getFromId(), comment.getPostId());
+            } else {
+                log.warn("userId={} has failed to add like to postId={}", comment.getFromId(), comment.getPostId());
+            }
+            return Response.ok().build();
+        } catch (RuntimeException e) {
+            log.warn("Error adding userId={} like to postId={}, error={}", comment.getFromId(), comment.getPostId(), e);
+            return Response.serverError().entity("Error adding like").build();
+        }
+    }
 }
